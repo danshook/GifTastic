0a1c20d68f7abb4f56752bc9cbc84fd347d49cfa
Space Mac <spacemac@Daniels-MacBook-Pro.local>
2018-03-09 16:34:24 -0700

    basic html done. buttons showing.

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -1,28 +1,109 @@
-// Event listener for search button
-$("#search").on("click", function() {
-  // Storing our giphy API URL
-  var queryURL =
-    "https://api.giphy.com/v1/gifs/random?api_key=dVQjq1REC3cKtfq4yAtBoiFMzxz2UxuX&tag=dancing dogs&q=&limit=10&rating=G";
+// **********************************************
+//             GLOBAL VARIABLES
+// **********************************************
 
-  // Perfoming an AJAX GET request to our queryURL
+// Initial array of topics
+var Characters = [
+  "bugs bunny",
+  "tasmanian devil",
+  "elmer fudd",
+  "yosemite sam",
+  "foghorn leghorn"
+];
+
+// displayCharacterInfo function re-renders the HTML to display the appropriate content
+// function displayCharacterInfo() {
+//   var character = $(this).attr("data-title");
+//   var queryURL =
+//     "https://api.giphy.com/v1/gifs/random?api_key=dVQjq1REC3cKtfq4yAtBoiFMzxz2UxuX&tag=" +
+//     character +
+//     "&rating=";
+
+function displayCharacterInfo() {
+  var character = $(this).attr("data-title");
+  var queryURL =
+    "https://api.giphy.com/v1/gifs/search?q=" +
+    character +
+    "&api_key=dVQjq1REC3cKtfq4yAtBoiFMzxz2UxuX";
+
+  // Creating an AJAX call for the specific character button being clicked
   $.ajax({
     url: queryURL,
     method: "GET"
-  })
+  }).then(function(response) {
+    // Creating a div to hold the character
+    var characterDiv = $("<div class='character'>");
 
-    // After the data from the AJAX request comes back
-    .then(function(response) {
-      // Saving the image_original_url property
-      var imageUrl = response.data.image_original_url;
+    // Storing the rating data
+    var rating = response.data.rating;
 
-      // Creating and storing an image tag
-      var Image = $("<img>");
+    // Creating an element to have the rating displayed
+    var pOne = $("<p>").text("Rating: " + rating);
 
-      // Setting the Image src attribute to imageUrl
-      Image.attr("src", imageUrl);
-      Image.attr("alt", "Image");
+    // Displaying the rating
+    characterDiv.append(pOne);
 
-      // Prepending the Image to the images div
-      $("#images").prepend(Image);
-    });
+    // Retrieving the URL for the image
+    var imgURL = response.data.images.original_still.url;
+
+    // Creating an element to hold the image
+    var image = $("<img>").attr("src", imgUrl);
+
+    // Appending the image
+    characterDiv.append(image);
+
+    // Putting the entire Character above the previous Characters
+    $("#characters-view").prepend(characterDiv);
+  });
+}
+
+// **********************************************
+//                  FUNCTIONS
+// **********************************************
+
+// Function for displaying character data
+function renderButtons() {
+  // Deleting the characters prior to adding new characters
+  // (This is necessary otherwise you will have repeat buttons)
+  $("#buttons-view").empty();
+
+  // Looping through the array of topics
+  for (var i = 0; i < Characters.length; i++) {
+    //
+    var a = $("<button>");
+    // Adding a class of character-btn to our button
+    a.addClass("character-btn");
+
+    // Adding a data attribute
+    a.attr("data-name", Characters[i]);
+
+    // Providing the initial button text
+    a.text(Characters[i]);
+
+    // Adding the button to the buttons-view div
+    $("#buttons-view").append(a);
+  }
+}
+// ***********************************************
+//                 MAIN PROCESS
+// ***********************************************
+// Event listener for search button
+$("#add-character").on("click", function(event) {
+  event.preventDefault();
+  console.log("CHARACTER " + character);
+  var character = $("#character-input")
+    .val()
+    .trim();
+
+  // Adding characters from the textbox to the array
+  Characters.push(character);
+
+  // Calling renderTopics
+  renderButtons();
 });
+
+// Adding a click event listener to all elements with a class of "character-btn"
+$(document).on("click", ".character-btn", displayCharacterInfo);
+
+// Calling the renderButtons function to display the initial buttons
+renderButtons();
